71405fe26f814ceec20fedab0ca3cdfc21c55033
manrick lazaro <77447723+Manrick-Lazaro@users.noreply.github.com>
2021-08-03 12:07:56 +0000

    03/08/2021 09:07

--- PROJETO/Usuario.cs
+++ PROJETO/Usuario.cs
@@ -27,6 +27,38 @@ class Usuario {
     return r;
   }
 
+  public Publicacao Listar (int ID) {
+    for (int i = 0; i < controlePublicacao; i++)
+      if (posts[i].GetId() == ID) 
+        return posts[i];
+    return null;
+  }
+
+  public void AtualizarPost (Publicacao x) {
+    Publicacao atualizacao = Listar(x.GetId());
+    if (atualizacao == null) 
+      return;
+    atualizacao.EditPost(x.GetPost());
+  }
+
+  private int Indice (Publicacao pub) {
+    for (int i = 0; i < controlePublicacao; i++){
+      if (posts[i] == pub) 
+        return i;
+    }
+    return -1;
+  }
+
+  public void ExcluirPost (Publicacao p) {
+    int i = Indice(p);
+    if (i == -1) return;
+    for (int j = i; j<controlePublicacao-1; j++) {
+      posts[j] = posts[j + 1];
+    }
+    controlePublicacao--;
+  }
+
+
   public void AddFriends (Usuario u) {
     if (controleAmigos == amigos.Length - 1) {
       Array.Resize(ref amigos, 2 * amigos.Length);
@@ -41,13 +73,7 @@ class Usuario {
     return r;
   } 
 
-  public Publicacao Listar (int ID) {
-    for (int i = 0; i < controlePublicacao; i++)
-      if (posts[i].GetId() == ID) 
-        return posts[i];
-    return null;
-  }
-
+  
   public void Curtir (int id, int x) {
     Publicacao curtir = Listar(id);
     if (x == 1) {
